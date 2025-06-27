195328a0dc9451c55b9efb8cda2272f9c65319e2
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-27 22:00:10 +0300

    authnav and usermenu css

--- src/components/AuthNav.jsx
+++ src/components/AuthNav.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom";
+import styles from "./AuthNav.module.css";
 
 const AuthNav = () => {
   return (
-    <nav>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Login</NavLink>
+    <nav className={styles.nav}>
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.active}` : styles.link
+        }
+      >
+        Register
+      </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.active}` : styles.link
+        }
+      >
+        Login
+      </NavLink>
     </nav>
   );
 };
--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,15 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../redux/auth/selectors.js";
 import { logOut } from "../redux/auth/operations.js";
+import styles from "./UserMenu.module.css";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
 
   return (
-    <div>
-      <p>Welcome, {user.name}</p>
-      <button onClick={() => dispatch(logOut())}>Logout</button>
+    <div className={styles.container}>
+      <p className={styles.greeting}>Welcome, {user.name}</p>
+      <button className={styles.logoutBtn} onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
     </div>
   );
 };
