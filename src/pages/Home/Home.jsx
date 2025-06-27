a9ff53f800440abe9a172ee939dfcde2dca1704b
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-06-27 22:11:46 +0300

    home.jsx render when user has logged in text replaced with welcome message

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
+import styles from "./Home.module.css";
+
 const Home = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
-    <section>
-      <h1>Welcome to the Contact Book App</h1>
-      <p>Please register or log in to access your contacts.</p>
-    </section>
+    <div className={styles.wrapper}>
+      {isLoggedIn ? (
+        <h2>Welcome back! You can now manage your contacts.</h2>
+      ) : (
+        <>
+          <h1>Welcome to the Contact Book App</h1>
+          <p>Please register or log in to access your contacts.</p>
+        </>
+      )}
+    </div>
   );
 };
 
